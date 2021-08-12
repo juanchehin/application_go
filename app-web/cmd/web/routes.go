ac36825dee8932a5fd61fc58d7cdd2ac1b48a0c8
chehin <chehinjuan@gmail.com>
2021-08-12 20:09:01 -0300

    Se instalo sessions

--- app-web/cmd/web/routes.go
+++ app-web/cmd/web/routes.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi"
@@ -13,13 +12,9 @@ import (
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
-	fmt.Printf("Entra routes \n")
-
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
-
-	fmt.Printf("Entra routes 1 \n")
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
