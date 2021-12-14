c011453498722acede95cf09e4fd4f72c0172029
chehin <chehinjuan@gmail.com>
2021-12-14 10:02:20 -0300

    Pinged database

--- test-connect/main.go
+++ test-connect/main.go
@@ -9,8 +9,22 @@ import (
 )
 
 func main() {
-	_, err := sql.Open("pgx", "host=localhost post=5432 dbname=test_connect user=postgres password=postgres")
+	// Conectarse a la base de datos
+	conn, err := sql.Open("pgx", "host=localhost port=5432 dbname=test_connect user=postgres password=postgres")
 	if err != nil {
 		log.Fatal(fmt.Sprintf("Unable to connect to database %v\n", err))
 	}
+
+	defer conn.Close()
+
+	log.Println("Connected to database!")
+
+	// Testear la conexion
+	err = conn.Ping()
+	if err != nil {
+		log.Fatal("Cannot ping database!  %v\n", err)
+		// log.Fatal(fmt.Sprintf(err))
+	}
+
+	log.Println("Pinged database!")
 }
