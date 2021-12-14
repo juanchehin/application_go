ac3ed5bbb661fb635d922ec897f47882ddce2bd4
chehin <chehinjuan@gmail.com>
2021-12-14 11:15:03 -0300

    obtencion de filas de la BD

--- test-connect/main.go
+++ test-connect/main.go
@@ -9,7 +9,9 @@ import (
 )
 
 func main() {
-	// Conectarse a la base de datos
+	// ======================================
+	// ==== Conectarse a la base de datos ===
+	// ======================================
 	conn, err := sql.Open("pgx", "host=localhost port=5432 dbname=test_connect user=postgres password=postgres")
 	if err != nil {
 		log.Fatal(fmt.Sprintf("Unable to connect to database %v\n", err))
@@ -19,7 +21,9 @@ func main() {
 
 	log.Println("Connected to database!")
 
-	// Testear la conexion
+	// ======================================
+	// === Testear la conexion ====
+	// ======================================
 	err = conn.Ping()
 	if err != nil {
 		log.Fatal("Cannot ping database!  %v\n", err)
@@ -27,4 +31,41 @@ func main() {
 	}
 
 	log.Println("Pinged database!")
+
+	// ======================================
+	// === Acceder a las filas de las tablas ===
+	// ======================================
+	err = getAllRows(conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getAllRows(conn *sql.DB) error {
+	rows, err := conn.Query("select id,first_name,last_name from users")
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer rows.Close()
+
+	var firstName, lastName string
+	var id int
+
+	for rows.Next() {
+		err := rows.Scan(&id, &firstName, &lastName)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		fmt.Println("Record is : ", id, firstName, lastName)
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatal("Error scanning rows", err)
+	}
+
+	fmt.Println("------------------")
+
+	return nil
 }
