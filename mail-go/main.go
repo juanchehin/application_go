3b6797fc673bd1e47bb6af2af8e8acbd2c0803bf
chehin <chehinjuan@gmail.com>
2022-05-02 16:47:45 -0300

    Funcion sendMsg

--- mail-go/main.go
+++ mail-go/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"models"
+	"mail-go/models"
+	mail "github.com/xhit/go-simple-mail/v2"
 )
 
 func main() {
@@ -15,11 +16,40 @@ func listenForMail() {
 	// Funcion asincronica (se ejecuta en segundo plano)
 	go func() {
 		for {
-			msg := <- 
+			// msg := <- app.MailChan
+			// msg := chan models.MailData
+			msg := make(chan models.MailData)
+			sendMsg(msg)
 		}
 	}
 }
 
-func sendMsg() {
-	
+func sendMsg(m models.MailData) {
+	server : mail.NewSMTOClient()
+	server.Hoost = "localhost"
+	server.Port = 1025
+	server.KeepAlive = false
+	server.ConnectTimeout = 10 * time.Second
+	server.SendTimeout = 10 * time.Second
+
+	client, err := server.Connect()
+	if err != nil {
+		errorLog.Println(err)
+	}
+
+	email := mail.NewMSG()
+	email.SetFrom(m.From).
+		AddTo(m.To).
+		AddCc(m.Subject).
+		SetSubject("New Go Email")
+
+	email.SetBody(mail.TextHTML, "Hello, <strong> World!</strong>!")
+
+	// Call Send and pass the client
+	err = email.Send(client)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("Email Sent")
+	}
 }
