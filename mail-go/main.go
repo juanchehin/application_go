a0fbd92a8034d6daf0071fe7d78ec439469a462b
chehin <chehinjuan@gmail.com>
2022-05-08 20:53:53 -0300

    email sent

--- mail-go/main.go
+++ mail-go/main.go
@@ -3,82 +3,63 @@ package main
 import (
 	"fmt"
 	"log"
-	"mail-go/internal/config"
-	"mail-go/models"
 	"time"
 
 	mail "github.com/xhit/go-simple-mail/v2"
 )
 
-var app config.AppConfig
-
 func main() {
-	fmt.Println("Hello, World!")
 
-	mailChan := make(chan models.MailData) // Creo el canal
-	app.MailChan = mailChan
-
+	fmt.Println("pasa -1")
 	listenForMail()
 
-	// app.MailChan <- msg
-	// mailChan <- msg // Envio 'msg' al channel 'mailChan'
-
-	defer close(app.MailChan) // Postergar hasta que se cierre main()
-	listenForMail()
-
-	msg := models.MailData{
-		To:      "chehin238@gmail.com",
-		From:    "sgmtucuman",
-		Subject: "Asunto - Golang",
-		Content: "",
-	}
-
-	app.MailChan <- msg
 }
 
 func listenForMail() {
-	// mailChan := make(chan models.MailData) // Creo el canal
-	// Funcion asincronica (se ejecuta en segundo plano)
-	// Bucle infinito en para levantar el server
-	go func() {
-		for {
-			msg := <-app.MailChan // Recibo desde 'mailChan' y asigno a 'msg'
-			// msg := chan models.MailData
-			// msg := make(chan models.MailData)
-			fmt.Println("msg ..", msg)
-			sendMsg(msg)
-		}
-	}()
-}
-
-func sendMsg(m models.MailData) {
 
 	server := mail.NewSMTPClient()
-	server.Host = "localhost"
-	server.Port = 1025
+
+	fmt.Println("pasa 0")
+
+	server.Host = "smtp.gmail.com"
+	server.Port = 587
+
+	server.Username = "sgmtucuman@gmail.com"
+	server.Password = ""
+	server.Encryption = mail.EncryptionSTARTTLS
+
 	server.KeepAlive = false
 	server.ConnectTimeout = 10 * time.Second
 	server.SendTimeout = 10 * time.Second
 
-	client, err := server.Connect()
+	// server.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	fmt.Println("pasa")
+	// SMTP client
+	smtpClient, err := server.Connect()
+
 	if err != nil {
-		// errorLog.Println(err)
-		log.Println(err)
+		log.Fatal(err)
 	}
 
 	email := mail.NewMSG()
-	email.SetFrom(m.From).
-		AddTo(m.To).
-		AddCc(m.Subject).
+	email.SetFrom("From Example <nube@example.com>").
+		AddTo("chehin238@gmail.com").
 		SetSubject("New Go Email")
 
 	email.SetBody(mail.TextHTML, "Hello, <strong> World!</strong>!")
 
+	// always check error after send
+	if email.Error != nil {
+		log.Fatal(email.Error)
+	}
+
 	// Call Send and pass the client
-	err = email.Send(client)
+	err = email.Send(smtpClient)
 	if err != nil {
 		log.Println(err)
 	} else {
 		log.Println("Email Sent")
 	}
+
 }
--- mail-go/models/models.go
+++ mail-go/models/models.go
@@ -2,7 +2,7 @@ package models
 
 import "html/template"
 
-// User is the user model
+//
 type MailData struct {
 	To      string
 	From    string
